2ff2efa5dad516e86ecd15d640c084aa5ee57162
S526555 <48032448+S526555@users.noreply.github.com>
2019-04-22 10:23:11 -0500

    Update Script.cmd

--- Script.cmd
+++ Script.cmd
@@ -1,4 +1,7 @@
 @PowerShell -ExecutionPolicy Bypass -Command Invoke-Expression $('$args=@(^&{$args} %*);'+[String]::Join(';',(Get-Content '%~f0') -notmatch '^^@PowerShell.*EOF$')) & goto :EOF
 npm install
 node app.js
-
+Start iexplore.exe
+$Browser=new-object -com internetexplorer.application
+$Browser.navigate2("192.168.0.1 :3000")
+$Browser.visible=$true
